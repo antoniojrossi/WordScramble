71208f90951bde731a14ee0889dfacf847d1825b
Antonio J Rossi <antoniojrossi@gmail.com>
2020-02-04 18:28:34 +0100

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Antonio J Rossi on 04/02/2020.
+//  Copyright © 2020 Antonio J Rossi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
